379e9861b1b08b3fefd7322c35ffd4794142dc6d
ranvijay1101 <xxranvijayxx@gmail.com>
2026-05-02 22:40:16 +0530

    Introduction to js

---
+++ operation.js
@@ -0,0 +1,7 @@
+let a= 1000000
+let b= 3.14159
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
---
+++ statement.js
@@ -0,0 +1,4 @@
+console.log("This is a statement acticity's statement.")
+let a= 6
+let b= 7
+console.log(b/a)
