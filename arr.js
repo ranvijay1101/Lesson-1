10383905034819f4e01ef40c96e5a8660e310eae
ranvijay1101 <xxranvijayxx@gmail.com>
2026-05-17 16:44:28 +0530

    index2

---
+++ arr.js
@@ -0,0 +1,12 @@
+// Arrays
+
+// let a = 10
+// let b = 20
+// let c = 30
+
+let fruits = ["apple", "mango", "orange", "banana"]
+
+console.log(fruits)
+
+console.log(fruits[1])
+console.log(fruits)
