601fa6ebd837016dac9916aaaf09a4c2a97d2a04
ranvijay1101 <xxranvijayxx@gmail.com>
2026-08-09 16:33:36 +0530

    python programmign

---
+++ python/introtopython.py
@@ -0,0 +1,14 @@
+#Activity 1
+
+print("Hello, World!")
+print("My name is Ranvijay")
+print(12)
+print(10 +20)
+print(91-24)
+
+#Activity 2
+
+print("Welcome to the world of Python programming!")
+print(4)
+import keyword
+print(keyword.kwlist)
