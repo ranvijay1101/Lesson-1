60db718015c471689fc4c1e6a64d27e468a1a90d
ranvijay1101 <xxranvijayxx@gmail.com>
2026-08-13 17:40:57 +0530

    adv

---
+++ python/adv.py
@@ -0,0 +1,31 @@
+snack_name="Chips"
+quantity = 3
+price = 25.50
+available = True
+print("Snack Name:", snack_name)
+print("Quantity:", quantity)
+print("Price:", price)
+print("Available:", available)
+total_price = price * quantity
+print("Total Price:", total_price)
+budget = 100
+affordable = total_price <= budget
+print("Budget:", budget)
+print("Affordable:", affordable)
+shop_type = "Snack"
+shop_place = "Shop"
+shop_name = shop_type + " " + shop_place
+print("Shop Name:", shop_name)
+first_letter = snack_name[0]
+print("First Letter of Snack Name:", first_letter)
+price1 = 30
+price2 = 50
+print("Before Swapping")
+print("Price 1:", price1)
+print("Price 2:", price2)
+temp = price1
+price1 = price2
+price2 = temp
+print("After Swapping")
+print("Price 1:", price1)
+print("Price 2:", price2)
